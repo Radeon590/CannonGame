023dd8dbf91138020ffa43496f9ed7c9d95bb14c
Rodion <59920979+Radeon590@users.noreply.github.com>
2024-06-17 10:35:25 +0300

    timer

---
+++ Assets/Scripts/ScoreAndTime/Timer.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Text timeText;
+
+    private float _timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        timeText.text = $"{Convert.ToInt32(_timer / 60)}:{Convert.ToInt32(_timer % 60)}";
+    }
+}
